53236d4dde3d23264feae76e47cc9ba03475caae
Nikhil Chadha <nikhilchadha828@gmail.com>
2026-04-30 20:44:55 +0800

    Cleaned Data Entry - Teaching file

--- backend/utils/excelParser.js
+++ backend/utils/excelParser.js
@@ -2,6 +2,42 @@ import XLSX from "xlsx";
 
 const selectedSheets = ["Report - Staff Member", "Data Entry - Teaching", "Data Entry - Assigned Role", "Data Entry - HDR",];
 
+const CleanDataEntryTeaching = (rows) => {
+  return rows
+    .filter((row) => row[""] && row["_1"] && row[""] !== "Unit Code")
+    .map((row) => {
+      return {
+        unitCode: row[""],
+        staffType: row[" <-- filter dropdowns -->"] || "",
+        staffName: row["_1"],
+        teachingScaledHours: row["Total Teaching Workload"] || 0,
+        teachingWLPoints: row["__EMPTY_5"] || 0,
+      };
+    });
+};
+
+const GroupingDataTeaching = (teachingRows) => {
+  const groupedData = {};
+
+  teachingRows.forEach((row) => {
+    if (!groupedData[row.staffName]) {
+      groupedData[row.staffName] = {
+        StaffName: row.staffName,
+        StaffType: row.staffType,
+        TotalTeachingScaledHours: 0,
+        TotalTeachingWLPoints: 0,
+        UnitCodes: [],
+      };
+    }
+
+    groupedData[row.staffName].TotalTeachingScaledHours += row.teachingScaledHours;
+    groupedData[row.staffName].TotalTeachingWLPoints += row.teachingWLPoints;
+    groupedData[row.staffName].UnitCodes.push(row.unitCode);
+  });
+
+  return Object.values(groupedData);
+};
+
 const parseExcel = (filePath) => {
   try {
     const workbook = XLSX.readFile(filePath);
@@ -15,6 +51,10 @@ const parseExcel = (filePath) => {
         parsedData[sheetName] = [];
       }
     });
+
+    if (parsedData["Data Entry - Teaching"]) {
+      parsedData["Data Entry - Teaching"] = GroupingDataTeaching(CleanDataEntryTeaching(parsedData["Data Entry - Teaching"]));
+    }
 
     return parsedData;
   } catch (error) {
